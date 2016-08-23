33cf559255d0826cdf0b59d5c18427af7ba7c8b4
Hao <a471558277@gmail.com>
2016-08-23 13:59:02 +0800

    Add

---
+++ Chapter 1/BinarySearch.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BinarySearch
+{
+    public class BinarySearch
+    {
+        public static int rank(int key,Array a)
+        {
+            int lo=0;
+            int hi-a.length
+        }
+    }
+}
